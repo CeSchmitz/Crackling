1590822288c28f25ef6d3d7b527cf0ead01f7399
Carl Schmitz <carl.schmitz@connect.qut.edu.au>
2022-02-17 10:35:52 +1000

    Changed from nose to pytest for testing in test_Constants.py

--- src/crackling/testing/test_Constants.py
+++ src/crackling/testing/test_Constants.py
@@ -1,35 +1,34 @@
-import nose.tools, nose
 from  crackling.Constants import *
 
 #######################
 ## Testing Constants ##
 #######################
 def test_CODE_ACCEPTED_value():
-    nose.tools.eq_(1, CODE_ACCEPTED, f'\nExpected:\t{1}\nActually:\t{CODE_ACCEPTED}')
+    assert 1 == CODE_ACCEPTED
 
 def test_CODE_REJECTED_value():
-    nose.tools.eq_(0, CODE_REJECTED, f'\nExpected:\t{0}\nActually:\t{CODE_REJECTED}')
+    assert 0 == CODE_REJECTED
 
 def test_CODE_AMBIGUOS_value():
-    nose.tools.eq_('-', CODE_AMBIGUOUS, f'\nExpected:\t{"-"}\nActually:\t{CODE_AMBIGUOUS}')
+    assert '-' == CODE_AMBIGUOUS
 
 def test_CODE_UNTESTED_value():
-    nose.tools.eq_('?', CODE_UNTESTED, f'\nExpected:\t{"?"}\nActually:\t{CODE_UNTESTED}')
+    assert '?' == CODE_UNTESTED
 
 def test_MODULE_10DB_value():
-    nose.tools.eq_('mm10db', MODULE_MM10DB, f'\nExpected:\t{"mm10db"}\nActually:\t{MODULE_MM10DB}')
+    assert 'mm10db' == MODULE_MM10DB
 
 def test_MODULE_SGRNASCORER2_value():
-    nose.tools.eq_('sgrnascorer2', MODULE_SGRNASCORER2, f'\nExpected:\t{"sgrnascorer2"}\nActually:\t{MODULE_SGRNASCORER2}')
+    assert 'sgrnascorer2' == MODULE_SGRNASCORER2
 
 def test_MODULE_CHOPCHOP_value():
-    nose.tools.eq_('chopchop', MODULE_CHOPCHOP, f'\nExpected:\t{"chopchop"}\nActually:\t{MODULE_CHOPCHOP}')
+    assert 'chopchop' == MODULE_CHOPCHOP
 
 def test_MODULE_CONSENSUS_value():
-    nose.tools.eq_('consensus', MODULE_CONSENSUS, f'\nExpected:\t{"consensus"}\nActually:\t{MODULE_CONSENSUS}')
+    assert 'consensus' == MODULE_CONSENSUS
 
 def test_MODULE_SPECIFICTY_value():
-    nose.tools.eq_('specificity', MODULE_SPECIFICITY, f'\nExpected:\t{"specificity"}\nActually:\t{MODULE_SPECIFICITY}')
+    assert 'specificity' == MODULE_SPECIFICITY
 
 def test_DEFAULT_GUIDE_PROPERTIES_value():
     expectedResults = {
@@ -60,7 +59,7 @@ def test_DEFAULT_GUIDE_PROPERTIES_value():
     'cfdOfftargetscore'         : CODE_UNTESTED,
     'passedAvoidLeadingT'       : CODE_UNTESTED,
     }
-    nose.tools.eq_(expectedResults, DEFAULT_GUIDE_PROPERTIES, f'\nExpected:\t{expectedResults}\nActually:\t{DEFAULT_GUIDE_PROPERTIES}')
+    assert expectedResults == DEFAULT_GUIDE_PROPERTIES
 
 def test_DEFAULT_GUIDE_PROPERTIES_ORDER_value():
     expectedResults = [
@@ -91,8 +90,4 @@ def test_DEFAULT_GUIDE_PROPERTIES_ORDER_value():
     'cfdOfftargetscore',
     'passedAvoidLeadingT',
     ]
-    nose.tools.eq_(expectedResults, DEFAULT_GUIDE_PROPERTIES_ORDER, f'\nExpected:\t{expectedResults}\nActually:\t{DEFAULT_GUIDE_PROPERTIES_ORDER}')
-
-
-if __name__ == '__main__':
-    nose.run()
+    assert expectedResults == DEFAULT_GUIDE_PROPERTIES_ORDER
